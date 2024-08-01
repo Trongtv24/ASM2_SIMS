a66d5bb77bef69b5ed6d86f6253d3679dac80309
Trongtv24 <trinhvantrong357@gmail.com>
2024-08-01 22:44:13 +0700

    final

---
+++ SIMS/Models/Score.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ASM2_SIMS.Models;
+
+public partial class Score
+{
+    public int ScoreId { get; set; }
+
+    public int? EnrollmentId { get; set; }
+
+    public decimal? Score1 { get; set; }
+
+    public virtual Enrollment? Enrollment { get; set; }
+}
